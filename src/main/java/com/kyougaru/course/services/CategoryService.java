576d79f18ad20aac58e8075d5e961efea93713a8
Kyougaru <gabrielterock@gmail.com>
2023-11-19 17:57:02 -0300

    Creating category

---
+++ src/main/java/com/kyougaru/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.kyougaru.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kyougaru.course.entities.Category;
+import com.kyougaru.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> category = repository.findById(id);
+		return category.get();
+	}
+}
